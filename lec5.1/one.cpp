bdab1d2a76d22c4b3f00124425a349348861a563
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2024-02-26 12:38:21 +0530

    C++ DSA

---
+++ lec5.1/one.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  //  int i=1;
+  //  while(i<=5){
+  //    cout << i <<endl;
+  //    i++;
+  //  }
+
+
+
+
+  // int i=1;
+  // do{
+  //   cout << i <<endl;
+  //   i++;
+  // }while(i<=10);
+
+
+
+  // for(int i=1;i<=10;i++){
+  //   cout<<i<<endl;
+  // }
+
+
+
+  // int n;
+  // int sum=0;
+
+  // cout << "enter numbers";
+  // cin >> n;
+  // for(int i=0; i<=n ;i++){
+  //    sum=sum+i;
+  // }
+  // cout << sum <<endl;
+   
+
+  
+}
