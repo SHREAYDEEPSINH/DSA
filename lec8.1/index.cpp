4192d84a54ca5fb055aba5287df7a9612596e6e0
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2024-03-11 11:56:30 +0530

    lec8.1

---
+++ lec8.1/index.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+
+// //qus 1 TNRN
+
+
+// void get_quote(){
+//    cout << "Nothing is IMpossible";
+// }
+
+// int main(){
+//   get_quote();
+
+//   return 0;
+// }
+
+
+
+
+// //qus 2 TNRN
+
+// void rahul(){
+//   cout << "my name is rahul" <<endl;
+//   cout << "my age is 18" <<endl;
+// }
+
+// int main(){
+//     rahul();
+
+//     return 0;
+// }
+
+
+
+
+// //qus 3 TSRN
+
+// void addition(int a,int b){
+//   cout << "Addition is " << a+b;
+// }
+
+// int main(){
+//     addition (5,3);
+
+//     return 0;
+// }
+
+
+
+// //qus 4 TSRN
+
+void addition(int a,int b){
+   cout <<"addition is " << a+b << endl;
+}
+
+void multi(int a,int b){
+   cout <<"addition is " << a*b << endl;
+}
+
+int main(){
+    addition(5,8);
+    multi(5,8);
+
+    return 0;
+}
+
