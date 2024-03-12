5c0d2a6bd7b38ee2f1ab6091402a091679f5b483
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2024-03-12 09:57:19 +0530

    lec8.2

---
+++ lec8.2/index.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+// // qus- TNRS 
+// int get_square(){
+//     int a=5;
+//     return a*a;
+// }
+
+// int main()
+// {
+//    cout<<"square:"<< get_square()<<endl;
+//    return 0;
+// }
+
+
+
+
+
+
+// // qus- TSRS 
+// int get_cude(int b){
+//     return b*b*b;
+// }
+
+// int main()
+// {
+//    cout<<"cube:"<< get_cude(4)<<endl;
+//    return 0;
+// }
+
+
+
+
+
+
+
+
+// // 18% GST 
+
+// int totalbill(int gst ,int amount){
+//     return amount+(amount*gst/100);
+// }
+// int main(){
+//     int gst=18;
+//     int amount=2000;
+
+//     int total=totalbill(gst,amount);
+//     cout << total;
+// }
+
+
+
+
+
+// // nested 
+
+// void shyam(){
+//     cout << "I am shyam " <<endl;
+// }
+// void ram(){
+//     cout << "I am Ram " <<endl;
+//     shyam();
+// }
+
+// int main(){
+//     ram();
+//     return 0;
+// }
+
+
+
+
+
+// // recursion
+
+// int fact(int n){
+//     if(n<=1){
+//         return 1;
+//     }else{
+//         return n*fact(n-1);
+//     }
+// }
+
+// int main(){
+//     cout << "factorial is: "<< fact(3);
+// }
